acd5cac21a86ab7373bceacb191245f9bba1dde2
Ahsun Tariq <ahsun.tariq@hotmail.com>
2024-05-01 15:19:21 -0700

    test pr

--- .github/workflows/scripts/find_channel.py
+++ .github/workflows/scripts/find_channel.py
@@ -4,13 +4,10 @@ def find_channel(team):
     with open('.github/workflows/slack/teams.txt', 'r') as f:
         for line in f:
             team_name, channel_number = line.strip().split(':')
-            print(f"Checking team: {team_name}")
             if team_name == team:
-                print(f"Found team: {team_name}")
                 return channel_number
     return ""
 
 team = os.getenv("TEAM")
-print(f"Team from environment: {team}")
 channel_number = find_channel(team)
-print(f"Channel number: {channel_number}")
+print(f"Channel number for team {team}: {channel_number}")
